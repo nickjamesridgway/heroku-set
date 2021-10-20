cf6566159e66727b64a3142e722d493a7c090f75
Nick James <nickjamesridgway@gmail.com>
2021-10-20 14:04:45 -0500

    Heroku

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.get('/',(req,res) =>
+    res.send('Hello')
+);
+
+app.listen(PORT, () => console.log(`Listening on PORT: PORT`));
